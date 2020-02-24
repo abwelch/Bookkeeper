46596fd8faf6a45c0aae9ebde53a7dc46ece4d10
abwelch <abw28@zips.uakron.edu>
2020-02-23 22:47:44 -0600

    Add model for login input

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookkeeper.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Username cannot exceed {1} characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Password cannot exceed 100 characters.")]
+        public string Password { get; set; }
+    }
+}
