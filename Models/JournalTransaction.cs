4181d4e3226696eb566e9059d92a071c796193c6
abwelch <abw28@zips.uakron.edu>
2020-03-01 15:30:58 -0600

    Add models for new sql tables

---
+++ Models/JournalTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bookkeeper.Models
+{
+    public partial class JournalTransaction
+    {
+        public JournalTransaction()
+        {
+            JournalEntries = new HashSet<JournalEntry>();
+        }
+
+        public int TransactionId { get; set; }
+        public string Memo { get; set; }
+        public DateTime RecordedDate { get; set; }
+        public TimeSpan RecordedTime { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int UserID { get; set; }
+
+        public virtual UserInfo User { get; set; }
+        public virtual ICollection<JournalEntry> JournalEntries { get; set; }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookkeeper.Models
+{
+    public partial class UserInfo
+    {
+        public UserInfo()
+        {
+            JournalTransactions = new HashSet<JournalTransaction>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public DateTime AccountCreation { get; set; }
+        public DateTime LastActivity { get; set; }
+        public int TotalCurrentTransactions { get; set; }
+        public int TotalStatements { get; set; }
+
+        public virtual ICollection<JournalTransaction> JournalTransactions { get; set; }
+    }
+}
