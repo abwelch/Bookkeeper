e7ad0b9d8406db630a1724e27551932c5586c97f
abwelch <abw28@zips.uakron.edu>
2020-03-08 22:01:51 -0500

    Add static class for common account names to use throughout application

---
+++ Models/GenericAccountNames.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookkeeper.Models
+{
+    public static class GenericAccountNames
+    {
+        #region [Balance Sheet]
+        // Assets
+        const string AccountsReceivable = "Accounts Receivable";
+        const string AccumulatedDepreciation = "Accumulated Depreciation";
+        const string AllowanceForDoubtfulAccounts = "Allowance for Doubtful Accounts";
+        const string Buildings = "Buildings";
+        const string Cash = "Cash";
+        const string Equipment = "Equipment";
+        const string Inventory = "Inventory";
+        const string Land = "Land";
+        const string Supplies = "Supplies";
+        // Liabilities
+        const string AccountsPayable = "Accounts Payable";
+        const string AccruedExpenses = "Accrued Expenses";
+        const string InstallmentLoansPayable = "Installment Loans Payable";
+        const string ShortTermLoansPayable = "Short-term Loans Payable";
+        const string UnearnedRevenue = "Unearned Revenue";
+        // Stockholder's Equity
+        const string CommonStock = "Common Stock";
+        const string PaidInCapitalInExcessCS = "Paid-in Capital in Excess of Par Value - Common Stock";
+        const string PaidInCapitalInExcessPS = "Paid-in Capital in Excess of Par Value - Preferred Stock";
+        const string PreferredStock = "Preferred Stock";
+        const string RetainedEarnings = "Retained Earnings";
+        #endregion [Balance Sheet]
+
+        #region [Income Statement]
+        // Operating Revenues
+        const string FeesEarned = "Fees Earned";
+        const string Sales = "Sales";
+        const string Services = "Services";
+        // Operating Expenses
+        const string CostOfGoodsSold = "Cost of Goods Sold";
+        const string Rent = "Rent";
+        const string Repairs = "Repairs";
+        const string Salaries = "Salaries";
+        const string Utilities = "Utilities";
+        #endregion [Income Statement]
+
+    }
+}
