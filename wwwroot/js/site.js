ed684a060de3571f1ea43bb0daa7c72eeeebe545
abwelch <abw28@zips.uakron.edu>
2020-03-20 22:19:38 -0500

    Add validation functions for line items

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Basic input validation for line items during transaction recording
+function ValidateLineItem() {
+    var accountName = document.getElementById("ItemAccountName");
+    if (accountName.value == "") {
+        window.alert("Error: A line item must contain an account name.");
+        return;
+    }
+    var debit = document.getElementById("ItemAmountDebit");
+    var credit = document.getElementById("ItemAmountCredit");
+    if (debit.value != "" && credit.value != "") {
+        window.alert("Error: A line item can only contain one amount.");
+        return;
+    }
+    if (debit.value == "" && credit.value == "") {
+        window.alert("Error: A line item must contain a debit or credit amount.");
+        return;
+    }
+}
 
-// Write your JavaScript code.
+// Default values of zero are passed in
+function ClearLineItemAmounts() {
+    document.getElementById("ItemAmountDebit").value = "";
+    document.getElementById("ItemAmountCredit").value = "";
+}
