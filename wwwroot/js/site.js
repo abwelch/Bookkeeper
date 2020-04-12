19cc018fe3a2d1d585d9e5b319ad9c488e9bd220
abwelch <abw28@zips.uakron.edu>
2020-04-12 11:22:22 -0500

    Expand transaction input validation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,7 @@ function ValidateLineItem() {
     var accountName = document.getElementById("ItemAccountName");
     if (accountName.value == "") {
         window.alert("Error: A line item must contain an account name.");
+        accountName.
         return;
     }
     var debit = document.getElementById("ItemAmountDebit");
@@ -15,10 +16,16 @@ function ValidateLineItem() {
         window.alert("Error: A line item must contain a debit or credit amount.");
         return;
     }
+    if (debit.value < 0 || credit.value < 0) {
+        window.alert("Error: Amounts cannot be negative.");
+        return;
+    }
 }
 
-// Default values of zero are passed in
+// Default values of zero are passed in and should be cleared for ValidateLineItem() to properly function
+// Also necessary to clear input after a line item is entered for next input
 function ClearLineItemAmounts() {
+    document.getElementById("ItemAccountName").value = "";
     document.getElementById("ItemAmountDebit").value = "";
     document.getElementById("ItemAmountCredit").value = "";
 }
