225fa72160a8cdc33b50c5a7bca612ae100d1b7a
abwelch <abw28@zips.uakron.edu>
2020-04-12 11:22:08 -0500

    Add migrations

--- Migrations/20200412160020_UpdateJournalEntry.Designer.cs
+++ Migrations/20200412160020_UpdateJournalEntry.Designer.cs
@@ -4,14 +4,16 @@ using Bookkeeper.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bookkeeper.Migrations
 {
     [DbContext(typeof(BookkeeperContext))]
-    partial class BookkeeperContextModelSnapshot : ModelSnapshot
+    [Migration("20200412160020_UpdateJournalEntry")]
+    partial class UpdateJournalEntry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
