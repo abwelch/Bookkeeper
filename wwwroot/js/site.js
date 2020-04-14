43ee5febc400660b8f9bac45c6a242a7235ca924
abwelch <abw28@zips.uakron.edu>
2020-04-14 13:34:07 -0500

    Add validation for edit line item and functon to set hidden value for corresponding button click

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,7 +3,6 @@ function ValidateLineItem() {
     var accountName = document.getElementById("ItemAccountName");
     if (accountName.value == "") {
         window.alert("Error: A line item must contain an account name.");
-        accountName.
         return;
     }
     var debit = document.getElementById("ItemAmountDebit");
@@ -22,10 +21,42 @@ function ValidateLineItem() {
     }
 }
 
+function ValidateEditLineItem() {
+    var accountName = document.getElementById("EditItemAccountName");
+    if (accountName.value == "") {
+        accountName.value = "DEFAULT_VALUE";
+        window.alert("Error: A line item must contain an account name.");
+        return;
+    }
+    var debit = document.getElementById("EditItemAmountDebit");
+    var credit = document.getElementById("EditItemAmountCredit");
+    if (debit.value != "" && credit.value != "") {
+        debit.value = 1;
+        credit.value = "";
+        window.alert("Error: A line item can only contain one amount.");
+        return;
+    }
+    if (debit.value == "" && credit.value == "") {
+        debit.value = 1;
+        window.alert("Error: A line item must contain a debit or credit amount.");
+        return;
+    }
+    if (debit.value < 0 || credit.value < 0) {
+        debit.value = 1;
+        credit.value = "";
+        window.alert("Error: Amounts cannot be negative.");
+        return;
+    }
+}
+
 // Default values of zero are passed in and should be cleared for ValidateLineItem() to properly function
 // Also necessary to clear input after a line item is entered for next input
 function ClearLineItemAmounts() {
     document.getElementById("ItemAccountName").value = "";
     document.getElementById("ItemAmountDebit").value = "";
     document.getElementById("ItemAmountCredit").value = "";
+}
+
+function SetActionItemIndex(element) {
+    element.value = 111; // arbitrarily chosen to avoid tampering
 }
