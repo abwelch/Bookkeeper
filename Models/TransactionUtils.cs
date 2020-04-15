5c9a6abdffc9785ad970a7353e75c2c6fd8657e8
abwelch <abw28@zips.uakron.edu>
2020-04-14 20:46:07 -0500

    Implement db retrievals with dapper

--- Models/TransactionUtils.cs
+++ Models/TransactionUtils.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Bookkeeper;
 using Bookkeeper.Data;
 using Bookkeeper.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace Bookkeeper.Models
 {
@@ -11,16 +15,18 @@ namespace Bookkeeper.Models
     public interface ITransactionUtils
     {
         bool ValidateTransaction(TransactionViewModel transaction);
-        bool CommitTransaction(TransactionViewModel transaction);
+        int CommitTransaction(TransactionViewModel transaction);
+        TransactionSummaryViewModel RetrieveLastUserCommitSummary(int userID);
     }
 
     public class TransactionUtils : ITransactionUtils
     {
         private readonly BookkeeperContext dbContext;
-
-        public TransactionUtils(BookkeeperContext _dbContext)
+        private readonly IDbConnection dbConnection;
+        public TransactionUtils(BookkeeperContext _dbContext, IDbConnection _dbConnection)
         {
             dbContext = _dbContext;
+            dbConnection = _dbConnection;
         }
 
         public bool ValidateTransaction(TransactionViewModel transaction)
@@ -45,29 +51,32 @@ namespace Bookkeeper.Models
             return false;
         }
 
-        public bool CommitTransaction(TransactionViewModel transaction)
+        public int CommitTransaction(TransactionViewModel transaction)
         {
+            int tranID = -1;
             decimal? total = 0;
             foreach (JournalLineItemViewModel lineItem in transaction.PreviousEntries)
             {
                 total += (lineItem.DebitAmount != null) ? lineItem.DebitAmount : 0;
             }
+
             JournalTransaction transactionEntry = new JournalTransaction()
             {
                 Memo = transaction.JournalHeader.Memo,
-                RecordedDate = transaction.JournalHeader.RecordedDate,
-                RecordedTime = transaction.JournalHeader.RecordedDate.TimeOfDay,
+                RecordedDateTime = DateTime.Now.ToString(),
                 TotalAmount = (decimal)total,
                 UserID = transaction.UserID
             };
             try
             {
                 dbContext.JournalTransactions.Add(transactionEntry);
+                dbContext.SaveChanges();
             }
             catch
             {
-                return false;
+                return -1;
             }
+            tranID = transactionEntry.TransactionId;
             foreach (JournalLineItemViewModel lineItem in transaction.PreviousEntries)
             {
                 JournalEntry entry = new JournalEntry()
@@ -76,7 +85,7 @@ namespace Bookkeeper.Models
                     DebitBalance = AccountsInfo.DebitAccounts.Contains(lineItem.AccountName),
                     DebitAmount = lineItem.DebitAmount,
                     CreditAmount = lineItem.CreditAmount,
-                    ParentTransactionId = transaction.UserID
+                    ParentTransactionId = transactionEntry.TransactionId
                 };
                 try
                 {
@@ -84,10 +93,40 @@ namespace Bookkeeper.Models
                 }
                 catch
                 {
-                    return false;
+                    return -1;
                 }
             }
-            return true;
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch
+            {
+                return -1;
+            }
+            return tranID;
+        }
+
+        public TransactionSummaryViewModel RetrieveLastUserCommitSummary(int tranID)
+        {
+            // Utilize dapper queries
+
+            string retrieveHeader = $"SELECT * FROM [Recording].JournalTransactions WHERE TransactionID = {tranID}";
+            var header = dbConnection.QueryFirstOrDefault<JournalTransaction>(retrieveHeader);
+            if (header == null)
+            {
+                return null;
+            }
+
+            string retrieveLineItems = $"SELECT * FROM [Recording].JournalEntries WHERE ParentTransactionID = {header.TransactionId}";
+            var lineItems = dbConnection.Query<JournalEntry>(retrieveLineItems);
+
+            TransactionSummaryViewModel mostRecentCommit = new TransactionSummaryViewModel()
+            {
+                JournalHeader = header,
+                LineItems = lineItems.ToList(),
+            };
+            return mostRecentCommit;
         }
     }
 }
