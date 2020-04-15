5a72ba596ba3a91f25f6f11aa23b690e6572b52c
abwelch <abw28@zips.uakron.edu>
2020-04-14 20:45:44 -0500

    Implement summary output

---
+++ Models/TransactionSummaryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookkeeper.Models
+{
+    public class TransactionSummaryViewModel
+    {
+        public JournalTransaction JournalHeader { get; set; }
+
+        public List<JournalEntry> LineItems { get; set; }
+
+    }
+}
