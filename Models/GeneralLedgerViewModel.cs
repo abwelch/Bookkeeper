4724294241952d507fa2dc28c7ef31eaa0b29ee6
abwelch <abw28@zips.uakron.edu>
2020-04-16 00:02:00 -0500

    Add model for transporting data to general ledger view

---
+++ Models/GeneralLedgerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookkeeper.Models
+{
+    public class GeneralLedgerViewModel
+    {
+        public Dictionary<string, LedgerAccount> AllAccounts { get; set; }
+    }
+
+    public class LedgerAccount
+    {
+        public bool IsDebit { get; set; }
+        public decimal Amount { get; set; }
+        public string AccountType { get; set; }
+    }
+}
