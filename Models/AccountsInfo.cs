fb4031c559bcd495fb932b52904c83cf97cfc62c
abwelch <abw28@zips.uakron.edu>
2020-04-16 00:01:44 -0500

    Add/alter for classifying general ledger accounts

--- Models/AccountsInfo.cs
+++ Models/AccountsInfo.cs
@@ -6,23 +6,53 @@ namespace Bookkeeper.Models
     {
         public static List<string> AccountNames { get; }
         public static List<string> DebitAccounts { get; }
-        public static List<string> CreditAccounts { get; }
-
+        public static Dictionary<string, string> AccountTypes { get; }
 
         static AccountsInfo()
         {
+            AccountTypes = new Dictionary<string, string>
+            {
+                { "Accounts Receivable", "Asset" },
+                { "Buildings", "Asset" },
+                { "Cash", "Asset" },
+                { "Equipment", "Asset" },
+                { "Inventory", "Asset" },
+                { "Supplies", "Asset" },
+                { "Accumulated Depreciation", "Contra Asset" },
+                { "Allowance for Doubtful Accounts", "Contra Asset" },
+                { "Accrued Expenses", "Liability" },
+                { "Installment Loans Payable", "Liability" },
+                { "Short-term Loans Payable", "Liability" },
+                { "Unearned Revenue", "Liability" },
+                { "Accounts Payable", "Liability" },
+                { "Common Stock", "Owner's Equity" },
+                { "Paid-in Capital in Excess of Par Value - Common Stock", "Owner's Equity" },
+                { "Paid-in Capital in Excess of Par Value - Preferred Stock", "Owner's Equity" },
+                { "Preferred Stock", "Owner's Equity" },
+                { "Retained Earnings", "Owner's Equity" },
+                { "Fees Earned", "Operating Revenue" },
+                { "Sales", "Operating Revenue" },
+                { "Services", "Operating Revenue" },
+                { "Cost of Goods Sold", "Operating Expense" },
+                { "Rent", "Operating Expense" },
+                { "Repairs", "Operating Expense" },
+                { "Salaries", "Operating Expense" },
+                { "Utilities", "Operating Expense" },
+            };
+
             AccountNames = new List<string>
             {
                 // Assets
                 "Accounts Receivable",
-                "Accumulated Depreciation",
-                "Allowance for Doubtful Accounts",
                 "Buildings",
                 "Cash",
                 "Equipment",
                 "Inventory",
                 "Land",
                 "Supplies",
+                // Contra Assets
+                "Accumulated Depreciation",
+                "Allowance for Doubtful Accounts",
                 // Liabilities
                 "Accrued Expenses",
                 "Installment Loans Payable",
@@ -58,24 +88,7 @@ namespace Bookkeeper.Models
                 "Inventory",
                 "Land",
                 "Supplies",
-                "Cost of Goods Sold"
-            };
-
-            CreditAccounts = new List<string>
-            {
-                "Accrued Expenses",
-                "Installment Loans Payable",
-                "Short-term Loans Payable",
-                "Unearned Revenue",
-                "Accounts Payable",
-                "Common Stock",
-                "Paid-in Capital in Excess of Par Value - Common Stock",
-                "Paid-in Capital in Excess of Par Value - Preferred Stock",
-                "Preferred Stock",
-                "Retained Earnings",
-                "Fees Earned",
-                "Sales",
-                "Services",
+                "Cost of Goods Sold",
                 "Rent",
                 "Repairs",
                 "Salaries",
