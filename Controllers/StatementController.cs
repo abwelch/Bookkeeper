54ee29a205bd9f899c2d011b595e8b4eebda1cc5
abwelch <abw28@zips.uakron.edu>
2020-04-16 13:02:49 -0500

    Create statement controller to handle overview and balancesheet

---
+++ Controllers/StatementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Bookkeeper.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookkeeper.Controllers
+{
+    public class StatementController : Controller
+    {
+        private readonly UserManager<IdentityUserExtended> userManager;
+        private readonly IStatementUtils statementUtils;
+        public StatementController(UserManager<IdentityUserExtended> _userManager, IStatementUtils _statementUtils)
+        {
+            userManager = _userManager;
+            statementUtils = _statementUtils;
+        }
+
+        public IActionResult Overview()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> BalanceSheet()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View(null);
+            }
+            IdentityUserExtended user = await userManager.GetUserAsync(User);
+            BalanceSheetViewModel model = statementUtils.GenerateBalanceSheet(user.UserInfoID);
+            return View(model);
+        }
+    }
+}
