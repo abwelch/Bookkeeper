69d41681c9b280d3c331a198cc76917b3282bbc6
abwelch <abw28@zips.uakron.edu>
2020-04-15 20:57:24 -0500

    Include method to update user total transactions

--- Models/UserInfoUtils.cs
+++ Models/UserInfoUtils.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using Bookkeeper.Data;
 using System.Threading.Tasks;
-
+using System.Data;
+using Dapper;
 
 namespace Bookkeeper.Models
 {
     public interface IUserInfoUtils
     {
         public UserInfo GetMostRecentUser();
+        public void IncrementTotalTransactions(int UserID);
     }
 
     public class UserInfoUtils : IUserInfoUtils
     {
         private readonly BookkeeperContext dbContext;
-        public UserInfoUtils(BookkeeperContext _dbContext)
+        private readonly IDbConnection dbConnection;
+        public UserInfoUtils(BookkeeperContext _dbContext, IDbConnection _dbConnection)
         {
             dbContext = _dbContext;
+            dbConnection = _dbConnection;
         }
         public UserInfo GetMostRecentUser() =>
             dbContext.UserInfos.OrderByDescending(p => p.UserID).FirstOrDefault();
+
+        public void IncrementTotalTransactions(int UserID)
+        {
+            string retrieveUser = $"SELECT * FROM [dbo].[UserInfos] WHERE UserID = {UserID}";
+            var user = dbConnection.QueryFirstOrDefault<UserInfo>(retrieveUser);
+            if (user != null)
+            {
+                user.TotalCurrentTransactions++;
+            }
+            dbContext.UserInfos.Update(user);
+            dbContext.SaveChanges();
+        }
     }
 }
